a3d63ab7673f87769a11be92cc964229c153e4d8
xiaokesama <1193274986@qq.com>
2024-11-17 23:12:27 +0800

    springboot 整合 minio

---
+++ springboot-mino/src/main/java/com/example/springbootmino/SpringbootMinoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootmino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMinoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMinoApplication.class, args);
+    }
+
+}
